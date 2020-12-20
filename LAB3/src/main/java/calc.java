f305f8a053bd9f1c0bc7dd65aa762655bfb224e2
Maciej Sochalski <maciejsochalski@gmail.com>
2020-12-20 19:44:31 +0100

    move project to intellij

--- LAB3/src/main/java/calc.java
+++ LAB3/src/main/java/calc.java
@@ -1,99 +1,97 @@
-package main.java;
-
 import java.util.Scanner;
 
 public class calc {
-	
-	public static double kwota; // Robimy dla prostoty na polach publicznych (mimo, ze sie nie powinno)
-	public static double kwota_zwrotu;
-	public static double rata;
-	public static double wynik;
-	public static double RataMal(double kwota, double opr, double okr) {
-		kwota_zwrotu = 0;
-		rata = 0;
-		int i = 1;
-		double podstawa_do_splaty = kwota/okr;
-		for (rata = 0; okr >= 1; okr--) {
-			rata = (((opr/100)*kwota)/12)+podstawa_do_splaty;
-			rata *= 100;
-			rata = Math.round(rata);
-			rata /= 100;
-			
-			System.out.println(i + ". Rata: " + rata + "zl");
-			
-			kwota_zwrotu = kwota_zwrotu + rata;
-			kwota_zwrotu *= 100;
-			kwota_zwrotu = Math.round(kwota_zwrotu);
-			kwota_zwrotu /= 100;
-			kwota = kwota - podstawa_do_splaty;
-			i++;
-		}
-		System.out.println("Kwota do splacenia: " + kwota_zwrotu + "zl");
-		return kwota_zwrotu;
-	}
-	
-	public static double RataSt(double kwota, double opr, double okr) {
-		kwota_zwrotu = 0;
-		rata = 0;
-		double q = 1 + ((opr/100)/12);
-		wynik = 1;
-		for(int i = 0; i < okr; i++)
-		{
-			wynik = wynik * q;
-		}
-		
-		q = q - 1;
-		
-		double wynik2 = wynik - 1;
-		
-		rata = kwota * wynik * q / wynik2;
-		kwota_zwrotu = rata * okr;
-		kwota_zwrotu *= 100;
-		kwota_zwrotu = Math.round(kwota_zwrotu);
-		kwota_zwrotu /= 100;
-		
-		rata *= 100;
-		rata = Math.round(rata);
-		rata /= 100;
-		
-		System.out.println("Rata: " + rata + "zl");
-		System.out.println("Kwota do splacenia: " + kwota_zwrotu + "zl");
-		return kwota_zwrotu;
-	}
-	
-	public static void main(String[] args) {
-		
-		System.out.println("Kalkulator kredytowy");
-		
-		System.out.print("Podaj kwote: ");
-		double kwota;
-		Scanner scan = new Scanner(System.in);
-		kwota = scan.nextDouble();
-		if (kwota <= 0)
-			throw new IllegalArgumentException("Kwota kredytu musi byc wieksza od 0!");
-		
-		System.out.print("Podaj oprocentowanie: ");
-		double opr;
-		opr = scan.nextDouble();
-		if (opr < 0)
-			throw new IllegalArgumentException("Oprocentowanie nie moze byc mniejsze od 0!");
-		
-		System.out.print("Podaj okres kredytu w miesiacach: ");
-		int okr;
-		okr = scan.nextInt();
-		if (okr <= 0)
-			throw new IllegalArgumentException("Okres kredytu musi byc wiekszy od 0!");
-		
-		System.out.print("Rata malejaca, czy stala? Dla malejacej wpisz 1, a dla stalej 2: ");
-		int rata_rodzaj;
-		rata_rodzaj = scan.nextInt();
-		
-		
-		if (rata_rodzaj == 1) {
-			RataMal(kwota, opr, okr);
-		}
-		else if (rata_rodzaj == 2) {
-			RataSt(kwota, opr, okr);
-		}
-	}
+
+    public static double kwota; // Robimy dla prostoty na polach publicznych (mimo, ze sie nie powinno)
+    public static double kwota_zwrotu;
+    public static double rata;
+    public static double wynik;
+    public static double RataMal(double kwota, double opr, double okr) {
+        kwota_zwrotu = 0;
+        rata = 0;
+        int i = 1;
+        double podstawa_do_splaty = kwota/okr;
+        for (rata = 0; okr >= 1; okr--) {
+            rata = (((opr/100)*kwota)/12)+podstawa_do_splaty;
+            rata *= 100;
+            rata = Math.round(rata);
+            rata /= 100;
+
+            System.out.println(i + ". Rata: " + rata + "zl");
+
+            kwota_zwrotu = kwota_zwrotu + rata;
+            kwota_zwrotu *= 100;
+            kwota_zwrotu = Math.round(kwota_zwrotu);
+            kwota_zwrotu /= 100;
+            kwota = kwota - podstawa_do_splaty;
+            i++;
+        }
+        System.out.println("Kwota do splacenia: " + kwota_zwrotu + "zl");
+        return kwota_zwrotu;
+    }
+
+    public static double RataSt(double kwota, double opr, double okr) {
+        kwota_zwrotu = 0;
+        rata = 0;
+        double q = 1 + ((opr/100)/12);
+        wynik = 1;
+        for(int i = 0; i < okr; i++)
+        {
+            wynik = wynik * q;
+        }
+
+        q = q - 1;
+
+        double wynik2 = wynik - 1;
+
+        rata = kwota * wynik * q / wynik2;
+        kwota_zwrotu = rata * okr;
+        kwota_zwrotu *= 100;
+        kwota_zwrotu = Math.round(kwota_zwrotu);
+        kwota_zwrotu /= 100;
+
+        rata *= 100;
+        rata = Math.round(rata);
+        rata /= 100;
+
+        System.out.println("Rata: " + rata + "zl");
+        System.out.println("Kwota do splacenia: " + kwota_zwrotu + "zl");
+        return kwota_zwrotu;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Kalkulator kredytowy");
+
+        System.out.print("Podaj kwote: ");
+        double kwota;
+        Scanner scan = new Scanner(System.in);
+        kwota = scan.nextDouble();
+        if (kwota <= 0)
+            throw new IllegalArgumentException("Kwota kredytu musi byc wieksza od 0!");
+
+        System.out.print("Podaj oprocentowanie: ");
+        double opr;
+        opr = scan.nextDouble();
+        if (opr < 0)
+            throw new IllegalArgumentException("Oprocentowanie nie moze byc mniejsze od 0!");
+
+        System.out.print("Podaj okres kredytu w miesiacach: ");
+        int okr;
+        okr = scan.nextInt();
+        if (okr <= 0)
+            throw new IllegalArgumentException("Okres kredytu musi byc wiekszy od 0!");
+
+        System.out.print("Rata malejaca, czy stala? Dla malejacej wpisz 1, a dla stalej 2: ");
+        int rata_rodzaj;
+        rata_rodzaj = scan.nextInt();
+
+
+        if (rata_rodzaj == 1) {
+            RataMal(kwota, opr, okr);
+        }
+        else if (rata_rodzaj == 2) {
+            RataSt(kwota, opr, okr);
+        }
+    }
 }
--- LAB3/src/test/java/test.java
+++ LAB3/src/test/java/test.java
@@ -1,42 +1,38 @@
-package test.java;
-
 import static org.junit.Assert.*;
 
 import org.junit.*;
 
-import main.java.calc;
-
 public class test {
-	private calc testCalc;
-	
-	@Before
-	public void beforeTest() {
-		testCalc = new calc();
-		System.out.println("Before");
-	}
-	
-	@After
-	public void afterTest() {
-		testCalc = null;
-		System.out.println("After");
-	}
-	
-	@Test
-	public void testCalc1() {
-		double result = testCalc.RataSt(5000, 10, 10);
-		assertEquals(5232.02, result, 0);
-	}
-	
-	@Test
-	public void testCalc2() {
-		double result1 = testCalc.RataSt(1231, 10, 12);
-		double result2 = testCalc.RataMal(1231, 10, 12);
-		assertNotSame(result1, result2);
-	}
-	
-	@Test
-	public void testCalc3() {
-		double result = testCalc.RataSt(5145, 10, 36);
-		assertNotNull("Error", result);
-	}
+    private calc testCalc;
+
+    @Before
+    public void beforeTest() {
+        testCalc = new calc();
+        System.out.println("Before");
+    }
+
+    @After
+    public void afterTest() {
+        testCalc = null;
+        System.out.println("After");
+    }
+
+    @Test
+    public void testCalc1() {
+        double result = testCalc.RataSt(5000, 10, 10);
+        assertEquals(5232.02, result, 0);
+    }
+
+    @Test
+    public void testCalc2() {
+        double result1 = testCalc.RataSt(1231, 10, 12);
+        double result2 = testCalc.RataMal(1231, 10, 12);
+        assertNotSame(result1, result2);
+    }
+
+    @Test
+    public void testCalc3() {
+        double result = testCalc.RataSt(5145, 10, 36);
+        assertNotNull("Error", result);
+    }
 }
